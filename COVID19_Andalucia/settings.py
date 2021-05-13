b9c8a26ae508c434c59e5c4346c223717675ff92
fjortegan <44491416+fjortegan@users.noreply.github.com>
2021-05-13 13:34:06 +0200

    Update settings.py

--- COVID19_Andalucia/settings.py
+++ COVID19_Andalucia/settings.py
@@ -146,6 +146,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+COMPRESS_ROOT = STATIC_ROOT
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
